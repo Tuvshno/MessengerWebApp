7d6cff546d8bdaf0153fd9b81ba55eae33d4507c
tuvshno <tuvshno@vt.edu>
2020-02-28 17:38:52 -0500

    Added serverside socket and client side socket

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ var express  = require('express')
 var socket = require('socket.io')
 
 var app = express()
+var server = app.listen(4000, function(){
+    console.log("listening to port 4000")
+})
 
-var server = app.listen(4000)
+app.use(express.static('public'))
 
 var io = socket(server)
 
-app.get('/', function(req, res){
-    console.log("recieved initial get request")
-    res.sendFile(__dirname + "/public/messenger.html")
-})
-
 io.on('connection', function(socket){
-    console.log("connected to the socket")
+    console.log("connected to the socket " + socket.id)
+
+    socket.on('message', function(data){
+        console.log(data)
+    })
 })
